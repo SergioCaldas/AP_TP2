7f03483ab507e0b6abb6308cf25b7dd56e4f47ac
filipecosta90 <filipecosta.90@gmail.com>
2016-04-25 19:03:56 +0100

    altered BLAS2LUPP

--- BLAS2LUPP.m
+++ BLAS2LUPP.m
@@ -1,24 +1,44 @@
-function A =BLAS2LUPP(A)
 % LU factorization with partial pivoting, overwriting L and U on A
 % see ALGORITHM 2.9 in Applied Numerical Linear Algebra, J. Demmel, SIAM
 % (2007), p.72
-% This differs from LUfact1 because A may not be square, has m rows and n
-% columns, with m>=n
+%
+% Copyright University of Minho (2016), Computer Science Dpt. 
+% HPC Group, Filipe Oliveira and Sergio Caldas
+%
 
-[m n]=size(A);
-L=eye(n); 
+function [ L U P ] = BLAS2LUPP(A)
+
+[m n] = size(A);
+
+%create diag matrix P
 P=eye(n); 
-U=A;
+
 for i=1:min(m-1,n)
+
     [~,p] = max(abs(A(i:n,i)));
     p = p+i-1;
+
+    % apply row permutations to A and P
     if p~=i
         A([i p],:) = A([p i], :);
+        P( [i p] , : ) =   P( [p i] , : );
     end
-    % apply row permutations to A and L
+
     A(i+1:m,i)=A(i+1:m,i)/A(i,i);
     if i<n
         A(i+1:m,i+1:n)=A(i+1:m,i+1:n)-A(i+1:m,i)*A(i,i+1:n);
     end
 end
+
+%divide A into L and U
+L=tril(A);
+U=triu(A);
+
+
+[ linesL colsL ] = size ( U );
+
+for pos=1:linesL
+    L ( pos,pos ) = 1;
+end
+
 end
